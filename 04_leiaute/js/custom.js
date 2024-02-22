c226e7f6e81016aa2da22d145bc7e9a152205e95
Paulo-SenacRS <prjunior@senacrs.com.br>
2024-02-22 15:30:47 -0300

    diagramação da UC

--- 04_leiaute/js/custom.js
+++ 04_leiaute/js/custom.js
@@ -87,31 +87,31 @@ $(document).ready(function () {
     btn_ambientes_08 = localStorage.getItem('btn_ambientes_08');
     btn_ambientes_09 = localStorage.getItem('btn_ambientes_09');
 
-    if (btn_ambientes_01.includes('check')) {
+    if (btn_ambientes_01 == 'check') {
         $(".btn-salas_abertas").css("background-image","url('imgs/painel/bt01_3.png')");
     }
-    if (btn_ambientes_02.includes('check')) {
+    if (btn_ambientes_02 == 'check') {
         $(".btn-salas_fechadas").css("background-image","url('imgs/painel/bt02_3.png')");
     }
-    if (btn_ambientes_03.includes('check')) {
+    if (btn_ambientes_03 == 'check') {
         $(".btn-salas_reunioes").css("background-image","url('imgs/painel/bt03_3.png')");
     }
-    if (btn_ambientes_04.includes('check')) {
+    if (btn_ambientes_04 == 'check') {
         $(".btn-recepcao").css("background-image","url('imgs/painel/bt04_3.png')");
     }
-    if (btn_ambientes_05.includes('check')) {
+    if (btn_ambientes_05 == 'check') {
         $(".btn-circulacoes").css("background-image","url('imgs/painel/bt05_3.png')");
     }
-    if (btn_ambientes_06.includes('check')) {
+    if (btn_ambientes_06 == 'check') {
         $(".btn-banheiros").css("background-image","url('imgs/painel/bt06_3.png')");
     }
-    if (btn_ambientes_07.includes('check')) {
+    if (btn_ambientes_07 == 'check') {
         $(".btn-copas").css("background-image","url('imgs/painel/bt07_3.png')");
     }
-    if (btn_ambientes_08.includes('check')) {
+    if (btn_ambientes_08 == 'check') {
         $(".btn-ambientes_descompressao").css("background-image","url('imgs/painel/bt08_3.png')");
     }
-    if (btn_ambientes_09.includes('check')) {
+    if (btn_ambientes_09 == 'check') {
         $(".btn-ambientes_especificos").css("background-image","url('imgs/painel/bt09_3.png')");
     }
     
--- 07_legislacao/js/custom.js
+++ 07_legislacao/js/custom.js
@@ -63,19 +63,25 @@ $(document).ready(function () {
     btn_normas_04 = localStorage.getItem('btn_normas_04');
     btn_normas_05 = localStorage.getItem('btn_normas_05');
 
-    if (btn_normas_01.includes('check')) {
+    alert('BTN 1: '+btn_normas_01);
+    alert('BTN 2: '+btn_normas_02);
+    alert('BTN 3: '+btn_normas_03);
+    alert('BTN 4: '+btn_normas_04);
+    alert('BTN 5: '+btn_normas_05);
+
+    if (btn_normas_01 == 'check'){
         $(".btn-nbr_16752_2020").css("background-image","url('objetos/painel02/bt_01_3.png')");
     }
-    if (btn_normas_02.includes('check')) {
+    if (btn_normas_02 == 'check'){
         $(".btn-nbr_16861_2020").css("background-image","url('objetos/painel02/bt_02_3.png')");
     }
-    if (btn_normas_03.includes('check')) {
+    if (btn_normas_03 == 'check'){
         $(".btn-nbr_6492_2021").css("background-image","url('objetos/painel02/bt_03_3.png')");
     }
-    if (btn_normas_04.includes('check')) {
+    if (btn_normas_04 == 'check'){
         $(".btn-nbr_17006_2021").css("background-image","url('objetos/painel02/bt_04_3.png')");
     }
-    if (btn_normas_05.includes('check')) {
+    if (btn_normas_05 == 'check'){
         $(".btn-abnt_nbr_17068_2022").css("background-image","url('objetos/painel02/bt_05_3.png')");
     }
     
